cdafe62aec431900e84a97bc7a31581f4cab1e70
mmackowsky <maciostah@gmail.com>
2024-02-22 10:51:41 +0100

    feat(schema): update

--- src/core/schema.py
+++ src/core/schema.py
@@ -1,13 +1,21 @@
 import graphene
+from graphql_auth.schema import UserQuery
+from graphql_auth import mutations
 
 import employees.schema
 
 
-class Query(employees.schema.Query, graphene.ObjectType):
+class Query(UserQuery, employees.schema.Query, graphene.ObjectType):
     pass
 
 
-class Mutation(employees.schema.Mutation, graphene.ObjectType):
+class AuthMutation(graphene.ObjectType):
+   register = mutations.Register.Field() #predefined settings to register user
+   verify_account = mutations.VerifyAccount.Field() #used to verify account
+   token_auth = mutations.ObtainJSONWebToken.Field() # get jwt to log in
+
+
+class Mutation(AuthMutation, employees.schema.Mutation, graphene.ObjectType):
     pass
 
 
